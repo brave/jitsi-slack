f4039bd3f2dd5ec3db47c4d1d2a4cf41fff185ce
Ben Huson <husobee@e-ie.io>
2020-05-15 10:37:35 -0400

    updates to random name to conform with agreed upon name, added test to show

--- room_name.go
+++ room_name.go
@@ -2,19 +2,30 @@ package jitsi
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"fmt"
+	"math/big"
+	"strings"
+)
+
+var (
+	roomAlphabet = []byte("abcdefghijklmnopqrstubwxyzABCDEFGHIJKLMNOPQRSTUBWXYZ0123456789-_")
+	roomLength   = 43
 )
 
 // RandomName will generate a new video name randomly.
 func RandomName() string {
-	// grab 256 bits randomly
-	b := make([]byte, 32)
-	_, err := rand.Read(b)
-	if err != nil {
-		// should not get here, if we do there is no more randomness
-		panic(fmt.Sprintf("error making room name: %s", err.Error()))
+	roomName := strings.Builder{}
+	for i := 0; i < 43; i++ {
+		j, err := rand.Int(rand.Reader, big.NewInt(int64(len(roomAlphabet)-1)))
+		if err != nil {
+			// should not get here, if we do there is no more randomness
+			panic(fmt.Sprintf("error making room name, randomness: %s", err.Error()))
+		}
+		err = roomName.WriteByte(roomAlphabet[int(j.Int64())])
+		if err != nil {
+			// should not get here, if we do there is no more randomness
+			panic(fmt.Sprintf("error making room name, name building: %s", err.Error()))
+		}
 	}
-	// return url safe base64 encoded string
-	return string(base64.URLEncoding.EncodeToString(b[:]))
+	return roomName.String()
 }
